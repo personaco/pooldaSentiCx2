8e4aa06b3db40ab8564f0315c69cabde6d61a3a0
personaco <kiere62@gmail.com>
2022-12-01 10:45:59 +0900

    package 폴더 위치 변경

---
+++ app/src/main/java/com/kiere/pooldasenticx2/NetworkRelatedClass/com/kiere/pooldasenticx2/NetworkRelatedClass/EventModel.java
@@ -0,0 +1,19 @@
+package com.kiere.pooldasenticx2.NetworkRelatedClass;
+
+public class EventModel {
+    private String eventTag;
+    private String message;
+
+    public EventModel(String eventTag, String message) {
+        this.eventTag = eventTag;
+        this.message = message;
+    }
+
+    public boolean isTagMatchWith(String tag){
+        return eventTag.equals(tag);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
